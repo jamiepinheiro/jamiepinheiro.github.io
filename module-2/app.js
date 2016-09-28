7a98178a654b29d5ee0d8813e8a008b7597dbdb6
Jamie Pinheiro <jamiepinheiro@gmail.com>
2016-09-28 19:16:52 -0400

    Module 2

--- module-2/app.js
+++ module-2/app.js
@@ -1,6 +1,5 @@
 (function() {
 	"use strict";
-
 	angular.module("ShoppingListCheckOff", [])
 	.controller("ToBuyController", ToBuyController)
 	.controller("AlreadyBoughtController", AlreadyBoughtController)
